7aae9d893f5baa2583ca9d9e9cbcdaa7cecc6751
Your Name <353844@whut.edu.cn>
2024-09-29 17:06:25 +0800

    lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,7 +1,9 @@
 package randomizedtest;
 
+import edu.princeton.cs.algs4.Alphabet;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+import timingtest.AList;
 import static org.junit.Assert.*;
 
 /**
@@ -9,4 +11,42 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+      AListNoResizing<Integer> l = new AListNoResizing<Integer>();
+      BuggyAList<Integer> r = new BuggyAList<>();
+
+      l.addLast(5);
+      l.addLast(10);
+      l.addLast(15);
+      r.addLast(5);
+      r.addLast(10);
+      r.addLast(15);
+
+      assertEquals(l.size(), r.size());
+
+      assertEquals(l.removeLast(), r.removeLast());
+      assertEquals(l.removeLast(), r.removeLast());
+      assertEquals(l.removeLast(), r.removeLast());
+    }
+
+    @Test
+    public void randomizedTest(){
+      AListNoResizing<Integer> L = new AListNoResizing<>();
+
+      int N = 500;
+      for (int i = 0; i < N; i += 1) {
+        int operationNumber = StdRandom.uniform(0, 2);
+        if (operationNumber == 0) {
+          // addLast
+          int randVal = StdRandom.uniform(0, 100);
+          L.addLast(randVal);
+          System.out.println("addLast(" + randVal + ")");
+        } else if (operationNumber == 1) {
+          // size
+          int size = L.size();
+          System.out.println("size: " + size);
+        }
+      }
+    }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,35 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<Integer>();
+        AList<Integer> ops = new AList<Integer>();
+        AList<Double> timelist = new AList<>();
+        Ns.addLast(1000);
+        Ns.addLast(2000);
+        Ns.addLast(4000);
+        Ns.addLast(8000);
+        Ns.addLast(16000);
+        Ns.addLast(32000);
+        Ns.addLast(64000);
+        Ns.addLast(128000);
+
+        for(int i=0;i<8;i++) ops.addLast(10000);
+
+        int M = 10000;
+        for(int i=0;i<Ns.size();i++) {
+            SLList<Integer> tmp = new SLList<>();
+            for(int j=0;j<Ns.get(i);j++) {
+                tmp.addLast(1);
+            }
+            Stopwatch timer = new Stopwatch();
+            for(int j=0;j<M;j++) {
+                tmp.getLast();
+            }
+            double timeInSeconds = timer.elapsedTime();
+            timelist.addLast(timeInSeconds);
+        }
+
+        printTimingTable(Ns, timelist, ops);
     }
 
 }
