d64530c8ec4fa61486305160943e8d0366cb1138
Your Name <353844@whut.edu.cn>
2024-11-04 18:04:31 +0800

    proj1

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,7 +3,7 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    Comparator<T> DequeComparator;
+    private final Comparator<T> DequeComparator;
 
     public MaxArrayDeque(Comparator<T> c) {
         DequeComparator = c;
